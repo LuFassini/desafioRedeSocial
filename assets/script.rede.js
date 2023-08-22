443499751eadfdaa107c9f0c242543d56f01502f
Luana Fassini <luanafassini.564@gmail.com>
2023-08-22 08:26:56 -0300

    feat: create class Dados and Informations

---
+++ assets/script.rede.js
@@ -0,0 +1,26 @@
+//user creation
+class Dados {
+    contructor (name,email,birthdate,address,phone,cpf) {
+    this.name = name;
+    this.email = email;
+    this.birthdate = birthdate;
+    this.address = address;
+    this.phone = phone;
+    this.cpf = cpf;
+}
+}
+
+//add user information 
+class informations {
+    constructor (name,email,birthdate,address,phone,cpf,age,sign,client) {
+        this.name = name;
+        this.email = email;
+        this.birthdate = birthdate;
+        this.address = address;
+        this.phone = phone;
+        this.cpf = cpf;
+        this.age = age;
+        this.sign = sign;
+        this.client = client;
+    }
+}
